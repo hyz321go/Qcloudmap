13c227f139807d16e34113c7332356aa9a89d3c7
hyz321go <hyz321go@163.com>
2024-03-15 14:43:57 +0800

    尝试使用矢量图层

--- qgisUtils/qgisLayerUtils.py
+++ qgisUtils/qgisLayerUtils.py
@@ -2,20 +2,18 @@ from qgis.core import QgsMapLayer,QgsRasterLayer,QgsVectorLayer
 import os
 import os.path as osp
 
-def readRasterFile(rasterFilePath):
-    rasterLayer = QgsRasterLayer(rasterFilePath,osp.basename(rasterFilePath))
-    return rasterLayer
+def readVectorFile(vectorFilePath):
+    vectorLayer = QgsVectorLayer(vectorFilePath,osp.basename(vectorFilePath),"ogr")
+    return vectorLayer
 
-def getRasterLayerAttrs(rasterLayer:QgsRasterLayer):
-    print("name: ", rasterLayer.name()) # 图层名
-    print("type: ", rasterLayer.type()) # 栅格还是矢量图层
-    print("height - width: ", rasterLayer.height(),rasterLayer.width()) #尺寸
-    print("bands: ", rasterLayer.bandCount()) #波段数
-    print("extent", rasterLayer.extent()) #外接矩形范围
-    print("source", rasterLayer.source()) #图层的源文件地址
-    print("crs", rasterLayer.crs())  # 图层的坐标系统
+def getVectorLayerAttrs(vectorLayer:QgsVectorLayer):
+    print("name: ", vectorLayer.name())  # 图层名
+    print("type: ", vectorLayer.type())  # 栅格还是矢量图层
+    print("extent", vectorLayer.extent())  # 外接矩形范围
+    print("source", vectorLayer.source())  # 图层的源文件地址
+    print("crs", vectorLayer.crs())  # 图层的坐标系统
 
 if __name__ == '__main__':
-    tifPath = r"G:\QGIS\myData\ca_nrc_CGG2013an83.tif"
-    tifLayer = readRasterFile(tifPath)
-    getRasterLayerAttrs(tifLayer)
+    shpPath = r"G:\QGIS\myData\naturalearth_lowres.shp"
+    shpLayer = readVectorFile(shpPath)
+    getVectorLayerAttrs(shpLayer)
